0daa56eaa4982cb43d3fcd9c1596662ae8d68888
QiHong Lu <lvqihong1992@gmail.com>
2019-06-23 20:10:34 -0400

    a function to figure out the dk label post hoc

--- src/examples/sequence-learning.py
+++ src/examples/sequence-learning.py
@@ -10,11 +10,11 @@ sns.set(style='white', palette='colorblind', context='talk')
 n_param, n_branch = 3, 2
 n_parts = 2
 n_samples = 5
-p_rm_ob_enc = 1
-p_rm_ob_rcl = 1
+p_rm_ob_enc = 0
+p_rm_ob_rcl = 0
 n_rm_fixed = False
 key_rep_type = 'time'
-sl = SequenceLearning(
+task = SequenceLearning(
     n_param, n_branch,
     p_rm_ob_enc=p_rm_ob_enc,
     p_rm_ob_rcl=p_rm_ob_rcl,
@@ -22,17 +22,17 @@ sl = SequenceLearning(
     key_rep_type=key_rep_type
 )
 # take sample
-X, Y = sl.sample(n_samples)
+X, Y = task.sample(n_samples)
 
-# plot a sample
+# pick a sample
 i = 0
 x, y = X[i], Y[i]
 
-# plot
+'''visualize the sample'''
 cmap = 'bone'
 f, axes = plt.subplots(
     1, 2, figsize=(8, 6),
-    gridspec_kw={'width_ratios': [sl.x_dim, sl.y_dim]}
+    gridspec_kw={'width_ratios': [task.x_dim, task.y_dim]}
 )
 axes[0].imshow(x, cmap=cmap)
 axes[1].imshow(y, cmap=cmap)
@@ -48,7 +48,30 @@ axes[0].set_ylabel('Time')
 n_timesteps = n_param
 for ax in axes:
     ax.axhline(n_timesteps-.5, color='red', linestyle='--')
-axes[0].axvline(sl.k_dim-.5, color='red', linestyle='--')
+axes[0].axvline(task.k_dim-.5, color='red', linestyle='--')
 
 f.savefig(f'examples/figs/seq-learn-rnn-{key_rep_type}.png',
           dpi=100, bbox_inches='tight')
+
+
+# '''figure out the dk label'''
+#
+#
+# def get_know_label(t, x, y):
+#     know = np.zeros((task.T_total,))
+#     # the time points where y_t is presented
+#     yt_present = x[:, t] == 1
+#     when_yt_present = np.where(yt_present)[0]
+#     # y labels at yt_present
+#     observed_y = x[yt_present, -task.y_dim:]
+#     for tau in range(np.sum(yt_present)):
+#         print(observed_y[tau, :])
+#         print(np.all(observed_y[tau, :] == y[t]))
+#         if np.all(observed_y[tau, :] == y[t]):
+#             know[when_yt_present[tau]:] = 1
+#     return know
+#
+#
+# # pick a time point
+# t = 0
+# np.array([get_know_label(t, x, y) for t in range(n_param)]).T
