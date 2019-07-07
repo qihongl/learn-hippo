7c4b44125fc3f81cfe15fa8789f09128b7b1b510
QiHong Lu <lvqihong1992@gmail.com>
2019-07-07 13:17:10 -0400

    revert the distribution over conditions

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -21,7 +21,7 @@ ALL_ENC_MODE = ['cum', 'disj']
 # conditions
 TZ_COND_DICT = bidict({0: 'RM', 1: 'DM', 2: 'NM'})
 # P_TZ_CONDS = [.25, .25, .5]
-P_TZ_CONDS = [.4, .4, .2]
+P_TZ_CONDS = [.25, .25, .5]
 # P_TZ_CONDS = [.5, .5, 0]
 TZ_CONDS = list(TZ_COND_DICT.values())
 
