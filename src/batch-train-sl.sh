7dc72af531c6897c1b69fc2cb34eb3fd2e2fe339
Qihong Lu <qlu@della5.princeton.edu>
2020-01-14 17:12:57 -0500

    updated slurm files

--- src/batch-train-sl.sh
+++ src/batch-train-sl.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-exp_name=penalty-random-discrete-highsim
+exp_name=penalty2pol-cmpt.9-lowsim-v1
 n_epoch=1000
 n_branch=4
 
 def_prob=.25
-similarity_max=.75
-similarity_min=.4
+n_def_tps=0
+
+similarity_max=.4
+similarity_min=0.0
 
 p_rm_ob_enc=0.3
 p_rm_ob_rcl=0.3
@@ -22,7 +24,7 @@ penalty_onehot=0
 
 normalize_return=1
 
-for subj_id in {0..1}
+for subj_id in {0..9}
 do
     for penalty in 0 4
     do
@@ -44,7 +46,7 @@ do
                                 ${pad_len} ${enc_size} \
                                 ${similarity_max} ${similarity_min} \
                                 ${penalty_random} ${penalty_discrete} ${penalty_onehot}\
-                                ${normalize_return} ${def_prob}
+                                ${normalize_return} ${def_prob} ${n_def_tps}
                         done
                     done
                 done
