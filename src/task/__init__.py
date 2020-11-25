37a1532ce9a2b09ad329c563b65a353e89b82c19
Qihong Lu <7212521+qihongl@users.noreply.github.com>
2020-11-24 20:16:17 -0500

    rm unused tasks

--- src/task/__init__.py
+++ src/task/__init__.py
@@ -1,7 +1,1 @@
-# from .Schema import Schema
-# from .StimSampler import StimSampler
-from .TwilightZone import TwilightZone
-from .ListLearning import ListLearning
 from .SequenceLearning import SequenceLearning
-from .ContextualChoice import ContextualChoice
-from .RNR import RNR
